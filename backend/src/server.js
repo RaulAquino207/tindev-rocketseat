e6a06af6c83f68e2ca822a6248bf0ba2e48b76ce
Raul Aquino <aquinoraul207@gmail.com>
2020-10-24 11:22:38 -0300

    started the project

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const port = 8080;
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    return res.json('Hello World');
+});
+
+server.listen(port);
